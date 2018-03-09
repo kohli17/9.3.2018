02706fab51ef6a7dda039fea1905d64a4064dc98
kohli17 <35589866+kohli17@users.noreply.github.com>
2018-03-09 15:49:59 +0530

    Create minimum.c
    print the minimum number

---
+++ minimum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a[20],i,min;
+	for(i=0;i<10;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	min=a[0];
+	for(i=0;i<10;i++)
+	{
+		if(a[i]<min)
+		{
+			min=a[i];
+		}
+	}
+	printf("%d",min);
+}
